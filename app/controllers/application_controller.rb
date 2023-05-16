9f64a484ed56f084804c827a271ee2a9da39e15b
teouoo <130281233+teouoo@users.noreply.github.com>
2023-05-16 19:11:41 +0900

    new.html.erbの修正
    hoge→nameの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :familyname, :firstname, :familyname_kana, :firsctname_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :familyname, :firstname, :familyname_kana, :firstname_kana, :birth_date])
   end
 
 end
