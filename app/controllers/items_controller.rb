6e13c5d7a58297cad12718abe842be2c570bbe39
teouoo <130281233+teouoo@users.noreply.github.com>
2023-05-17 21:08:42 +0900

    出品のためのコントローラとビューの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,29 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
+    # @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  
+    if @item.save
+      # 保存に成功した場合の処理
+      redirect_to root_path
+    else
+      # 保存に失敗した場合の処理
+      render :new
+    end
+  end
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_text, :category_id, :item_status_id, :delivery_charge_id,:delivery_area_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
+
+
 end
