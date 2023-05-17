512da9fce92192d4049614d2921194e3d841af18
teouoo <130281233+teouoo@users.noreply.github.com>
2023-05-17 16:57:02 +0900

    active hashのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_area
+  belongs_to :delivery_charge
+  belongs_to :delivery_time
+  belongs_to :item_status
+
+  #空の投稿を保存できないようにする
+  validates :category, :delivery_area, :delivery_charge, :delivery_time, :item_status, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+  
 end
