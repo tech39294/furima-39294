9358e669d811cc090154ab3d53750accedb00753
teouoo <happpyprogrammming@gmail.com>
2023-05-23 15:49:26 +0900

    rubocopによる修正結果2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new]
+
   def index
-    @items = Item.order("created_at DESC")
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -10,18 +11,18 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-  
+
     if @item.save
       redirect_to root_path
     else
       render :new
     end
   end
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_text, :category_id, :item_status_id, :delivery_charge_id,:delivery_area_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_text, :category_id, :item_status_id, :delivery_charge_id,
+                                 :delivery_area_id, :delivery_time_id, :price).merge(user_id: current_user.id)
   end
-
-
 end
