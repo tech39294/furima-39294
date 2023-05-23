b0cbb5a670f65d54521876c8cdb60e52c397a521
teouoo <happpyprogrammming@gmail.com>
2023-05-23 15:48:27 +0900

    rubocopによる修正結果

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,20 +9,22 @@ class Item < ApplicationRecord
   belongs_to :delivery_time
   belongs_to :item_status
 
-  #空の投稿を保存できないようにする
-  validates :item_name, :item_text, :category, :delivery_area, :delivery_charge, :delivery_time, :item_status, :price, presence: true
+  # 空の投稿を保存できないようにする
+  validates :item_name, :item_text, :category, :delivery_area, :delivery_charge, :delivery_time, :item_status, :price,
+            presence: true
   validates :image, presence: true
 
-  #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :item_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  # ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  #価格は、¥300~¥9,999,999の間のみ保存可能であること。
-  #価格は半角数値のみ保存可能であること。
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be a number between ¥300 and ¥9,999,999" }
-  validates :price, format: { with: /\A[0-9]+\z/, message: "must be a half-width number" }
-  
+  # 価格は、¥300~¥9,999,999の間のみ保存可能であること。
+  # 価格は半角数値のみ保存可能であること。
+  validates :price,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'must be a number between ¥300 and ¥9,999,999' }
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'must be a half-width number' }
 end
