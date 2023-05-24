67042fccc8fa8a23823b2063b32a91054e514a42
teouoo <happpyprogrammming@gmail.com>
2023-05-24 19:07:43 +0900

    フォーム実装の一部修正
    コントローラーとルーティングとビューを修正した

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
   end
@@ -9,14 +11,17 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_address_params
     params.require(:order_address).permit(:postal_code, :delivery_area_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
-  
 end
