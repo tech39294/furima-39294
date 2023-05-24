2d8dec3ac8912e1536d5aabb0c731372b4cec415
teouoo <happpyprogrammming@gmail.com>
2023-05-24 18:18:02 +0900

    form実装のためのコントローラーとビューの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :delivery_area_id, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+  
 end
