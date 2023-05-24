c7812cefb74672abc94646f8fc7cadc672af50cc
teouoo <happpyprogrammming@gmail.com>
2023-05-25 00:53:50 +0900

    dbの更新
    addressesテーブルが不完全な状態でmigrateされていたので、rollback後に再度migrateしたところ、schemaが変わった。

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,16 @@ ActiveRecord::Schema.define(version: 2023_05_24_083246) do
   end
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "postal_code", null: false
+    t.integer "delivery_area_id", null: false
+    t.string "city", null: false
+    t.string "street_address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -81,6 +89,7 @@ ActiveRecord::Schema.define(version: 2023_05_24_083246) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
